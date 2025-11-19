0f24cd2c04f2b04ea96b8a1082c7e3c62fabe3e1
Kazan <andyswebking@gmail.com>
2025-11-19 08:10:34 +0800

    Reset build and set redirecting

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/landing",
+  base: "/landing/",
   build: {
-    outDir: "docs",
+    outDir: "docs/landing",
   },
 });
